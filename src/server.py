bb888016a8011a8a72adf448670f089b769f172b
lilmond <54730301+lilmond@users.noreply.github.com>
2022-02-16 20:20:27 +0800

    Update server.py

--- src/server.py
+++ src/server.py
@@ -9,16 +9,20 @@ SRC_LISTEN_PORT = 4444
 # where client.py will connect
 DST_LISTEN_HOST = "0.0.0.0"
 DST_LISTEN_PORT = 4445
-DST_HOST = "127.0.0.1" # server will refuse to accept connection not coming from this ip
+DST_HOST = "168.234.83.163" # server will refuse to accept connection that is not coming from this ip. this is the only way it verifies if its you
 
 dst_sock = None
 
 def server_listener(listen_host, listen_port, func_pass):
-    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_sock.bind((listen_host, listen_port))
-    server_sock.listen()
-    print(f"notice: listening to {listen_host}:{listen_port}")
+    try:
+        server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_sock.bind((listen_host, listen_port))
+        server_sock.listen()
+        print(f"notice: listening to {listen_host}:{listen_port}")
+    except Exception as e:
+        print(f"error: unable to listen to {listen_host}:{listen_port} e: {e}")
+        return
 
     while True:
         client_sock, client_address = server_sock.accept()
@@ -42,6 +46,11 @@ def src_handle(sock):
 def dst_handle(sock):
     sock_ip, sock_port = sock.getpeername()
     sock_address = f"{sock_ip}:{sock_port}"
+
+    if not sock_ip == DST_HOST:
+        print(f"warning: DST_HOST is not {sock_address}")
+        sock.close()
+        return
 
     print(f"notice: dst connected: {sock_address}")
 
@@ -95,16 +104,19 @@ def packet_router(src, dst):
         return
 
     finally:
-        print(f"notice: ended packet router {src_address} -> {dst_address}")
         src.close()
         dst.close()
+        print(f"notice: ended packet router {src_address}:{src_port} -> {dst_address}:{dst_port}")
 
 def main():
     threading.Thread(target=server_listener, args=[SRC_LISTEN_HOST, SRC_LISTEN_PORT, src_handle], daemon=True).start()
     threading.Thread(target=server_listener, args=[DST_LISTEN_HOST, DST_LISTEN_PORT, dst_handle], daemon=True).start()
 
-    while True:
-        pass
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        return
 
 if __name__ == "__main__":
     main()
